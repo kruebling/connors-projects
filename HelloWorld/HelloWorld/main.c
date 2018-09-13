9fa0708c9171fd70c8b4faadab143256cc2ca5d2
kruebling <keegan.ruebling@gmail.com>
2018-09-13 16:13:18 -0700

    first test

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 /* C code main */
+/* Make the console say "HelloWorld" */
+/* Dont overthink it, even if you don't know some of this */
+/* Press F5 to run your code */
+/* When it works, follow the instructions in the README to push it back up */
 
 void main()
 {
-	printf("Hello World\n");
+	char test[] = "HelloConnor";
+
+	if (strcmp(test, "HelloWorld") != 0)
+	{
+		return;
+	}
+	else 
+	{
+		printf("%s\n", test);
+	}
+
+
+	//ignore this, it adds the press any key to continue
+	system("pause");
 }
